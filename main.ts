afe250d2df42ba972517cf873727eb78f003e3c2
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-04-14 11:41:30 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let air_quality = 0
+radio.setGroup(11)
+radio.setTransmitPower(7)
 basic.forever(function () {
-	
+    air_quality = 0
+    radio.sendValue("air quality", air_quality)
+})
+basic.forever(function () {
+    basic.showNumber(air_quality)
 })
